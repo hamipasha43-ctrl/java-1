93e8da5bed6a8a00f2fbd2b37ebd5cff9cc5a98b
hamipasha43-ctrl <hamipasha43@gmail.com>
2026-08-21 20:12:13 +0500

    created

---
+++ chapter17to20/app.js
@@ -0,0 +1,42 @@
+//QUESTION NUM 1//
+let arr = [[], []];
+
+console.log(arr);
+
+//QUESTION NUM 2//
+let matrix = [
+    [0, 1, 2, 3],
+    [1, 0, 1, 2],
+    [2, 1, 0, 1]
+];
+
+console.log(matrix);
+
+
+//QUESTION NUM 3//
+for (let i = 1; i <= 10; i++) {
+    console.log(i);
+}
+
+//QUESTION NUM 4//
+let num = +prompt("Enter table number:");
+let length = +prompt("Enter table length:");
+
+for (let i = 1; i <= length; i++) {
+    console.log(num + " x " + i + " = " + (num * i));
+}
+//QUESTION NUM 5//
+
+let fruits = ["apple", "banana", "mango", "orange", "strawberry"];
+
+for (let i = 0; i < fruits.length; i++) {
+    console.log(fruits[i]);
+}
+//QUESTION NUM 6//
+
+//QUESTION NUM 7//
+//QUESTION NUM 8//
+
+//QUESTION NUM 9//
+//QUESTION NUM 10//
+
