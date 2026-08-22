52db27a417f2e3bbcd30e2a8c2f3b4e395c6065c
hamipasha43-ctrl <hamipasha43@gmail.com>
2026-08-22 20:25:58 +0500

    file

--- chapter17to20/app.js
+++ chapter17to20/app.js
@@ -33,10 +33,76 @@ for (let i = 0; i < fruits.length; i++) {
     console.log(fruits[i]);
 }
 //QUESTION NUM 6//
+// A//
+for (var i = 1; i <= 15; i++) {
+    document.write(i + ", ");
+}
+
+//B//
+for (var i = 10; i >= 1; i--) {
+    document.write(i + ", ");
+}
+//C//
+for (var i = 0; i <= 20; i += 2) {
+    document.write(i + ", ");
+}
+//D//
+for (var i = 1; i <= 19; i += 2) {
+    document.write(i + ", ");
+}
+//E//
+for (var i = 2; i <= 20; i += 2) {
+    document.write(i + "k, ");
+}
+
+
 
 //QUESTION NUM 7//
+var A = ["cake", "apple pie", "cookie", "chips", "patties"];
+
+var search = prompt("What do you want to search?");
+
+var found = false;
+
+for (var i = 0; i < A.length; i++) {
+    if (A[i] === search) {
+        found = true;
+        break;
+    }
+}
+
+if (found) {
+    alert(search + " is found in the list.");
+} else {
+    alert(search + " is not found in the list.");
+}
 //QUESTION NUM 8//
 
-//QUESTION NUM 9//
-//QUESTION NUM 10//
+var A = [24, 53, 78, 91, 12];
 
+var largest = A[0];
+
+for (var i = 1; i < A.length; i++) {
+    if (A[i] > largest) {
+        largest = A[i];
+    }
+}
+
+console.log("Largest number is: " + largest);
+//QUESTION NUM 9//
+var A = [24, 53, 78, 91, 12];
+
+var smallest = A[0];
+
+for (var i = 1; i < A.length; i++) {
+    if (A[i] < smallest) {
+        smallest = A[i];
+    }
+}
+console.log("Smallest number is: " + smallest);
+
+
+//QUESTION NUM 10//
+for (var i = 5; i <= 100; i += 5) {
+    console.log(i);
+}
