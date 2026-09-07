98a32c9810b063962ee0979e964be167335ae85d
hamipasha43-ctrl <hamipasha43@gmail.com>
2026-09-07 20:31:14 +0500

    recreated

---
+++ chapter26to30/app.js
@@ -0,0 +1,28 @@
+//QUESTION NUM 1//
+
+//A//
+var number = prompt("Enter a positive integer:");
+document.write("Number: " + number);
+
+//B//
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//QUESTION NUM 1//
+//QUESTION NUM 1//
+//QUESTION NUM 1//
+//QUESTION NUM 1//
+//QUESTION NUM 1//
+//QUESTION NUM 1//
+//QUESTION NUM 1//
